c5f16f5cf0426073340f0e6f8748795900a64d74
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-04-30 21:08:51 -0700

    Function num1 array makes first number
    Basis for calculator is to allow users to enter 2 numbers.
    First will be num1 which can be an array of numbers.
    Appending a string may be easier.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let num1, num2, operator;
+const output = document.querySelector(".output-text");
+const numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numArray.forEach((num) => document.querySelector(num))
+
+// function to put 1st number in output
+
+function num1(arr) {
+  arr.forEach((num) =>{
+    num.addEventSelector("click", () => {
+      output.append(num);
+    })
+  })
+}
 
 function add(n1, n2) {
   return n1 + n2;
